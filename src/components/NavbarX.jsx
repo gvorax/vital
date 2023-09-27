c9bd00422095cab5b51fa8aaed23168dec98019f
gvorax <otabek83019@gmail.com>
2023-09-27 23:29:19 +0500

    fixed

--- src/components/NavbarX.jsx
+++ src/components/NavbarX.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/logo.png";
 import { useTranslation } from "react-i18next";
 import UzbekFlag from "../assets/uzbek.jpg";
 import RussianFlag from "../assets/rus.png";
-import UsaFlag from '../assets/usa.png';
+import UsaFlag from "../assets/usa.png";
 import "../i18n/i18n";
 import { Link } from "react-scroll";
 
@@ -106,65 +106,58 @@ const NavbarX = () => {
             </Link>
           </ul>
           <div className="lng">
-          <div className="dropdown">
-            {localStorage.getItem("language") == "uz" ? (
-              <div className="lan">
-                <img src={UzbekFlag} alt="" />
-                <button className="dropbtn">Uzbek</button>
-              </div>
-            ) : (
-              null
-            )}
-            {localStorage.getItem("language") == "rus" ? (
-               <div className="lan">
-               <img src={RussianFlag} alt="" />
-               <button className="dropbtn">Русский</button>
-             </div>
-            ) : (
-              null
-            )}
-           {localStorage.getItem("language") == "eng" ? (
-               <div className="lan">
-               <img src={UsaFlag} alt="" />
-               <button className="dropbtn">English</button>
-             </div>
-            ) : (
-              null
-            )}
+            <div className="dropdown">
+              {localStorage.getItem("language") == "uz" ? (
+                <div className="lan">
+                  <img src={UzbekFlag} alt="" />
+                  <button className="dropbtn">Uzbek</button>
+                </div>
+              ) : null}
+              {localStorage.getItem("language") == "rus" ? (
+                <div className="lan">
+                  <img src={RussianFlag} alt="" />
+                  <button className="dropbtn">Русский</button>
+                </div>
+              ) : null}
+              {localStorage.getItem("language") == "eng" ? (
+                <div className="lan">
+                  <img src={UsaFlag} alt="" />
+                  <button className="dropbtn">English</button>
+                </div>
+              ) : null}
 
-            <div className="dropdown_content">
-              <a
-                href="#"
-                onClick={(e) => {
-                  changeLanguage("uz");
-                }}
-              >
-                <img src={UzbekFlag} alt="" />
-                <p> Uzbek</p>
-              </a>
-              <a
-                href="#"
-                onClick={(e) => {
-                  changeLanguage("rus");
-                }}
-              >
-                <img src={RussianFlag} alt="" />
-                <p> Русский</p>
-              </a>
-              <a
-                href="#"
-                onClick={(e) => {
-                  changeLanguage("eng");
-                }}
-              >
-                <img src={UsaFlag} alt="" />
-                <p> English</p>
-              </a>
+              <div className="dropdown_content">
+                <a
+                  href="#"
+                  onClick={(e) => {
+                    changeLanguage("uz");
+                  }}
+                >
+                  <img src={UzbekFlag} alt="" />
+                  <p> Uzbek</p>
+                </a>
+                <a
+                  href="#"
+                  onClick={(e) => {
+                    changeLanguage("rus");
+                  }}
+                >
+                  <img src={RussianFlag} alt="" />
+                  <p> Русский</p>
+                </a>
+                <a
+                  href="#"
+                  onClick={(e) => {
+                    changeLanguage("eng");
+                  }}
+                >
+                  <img src={UsaFlag} alt="" />
+                  <p> English</p>
+                </a>
+              </div>
             </div>
           </div>
         </div>
-        </div>
-        
       </div>
     </nav>
   );
